5fa42842fae13f9b8a7a5ab046ce8af4493fc593
frothywater <kokoroic@outlook.com>
2021-05-22 23:30:11 +0800

    feat: Util dynamic_unique_ptr_cast

---
+++ include/interpreter/util/Util.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+struct Util {
+    template<typename Derived, typename Base, typename Del>
+    static std::unique_ptr<Derived, Del>
+    dynamic_unique_ptr_cast(std::unique_ptr<Base, Del> &&p) {
+        if (Derived *result = dynamic_cast<Derived *>(p.get())) {
+            p.release();
+            return std::unique_ptr<Derived, Del>(result, std::move(p.get_deleter()));
+        }
+        return std::unique_ptr<Derived, Del>(nullptr, p.get_deleter());
+    }
+};
