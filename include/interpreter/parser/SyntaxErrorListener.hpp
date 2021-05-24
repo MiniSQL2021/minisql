927eb7582f1ac79ca36e3ed65ff1c7958d2e3756
frothywater <kokoroic@outlook.com>
2021-05-24 10:55:12 +0800

    fix: Rewrite SyntaxError

--- include/interpreter/parser/SyntaxErrorListener.hpp
+++ include/interpreter/parser/SyntaxErrorListener.hpp
@@ -5,18 +5,18 @@
 
 #include "antlr4-runtime.h"
 
-class QueryParserSyntaxError : public std::exception {
-    std::string message;
+struct SyntaxError : public std::exception {
+    const size_t line, pos;
+    const std::string msg;
+    const bool hitEOF;
 
-public:
-    QueryParserSyntaxError(size_t line, size_t position, const std::string &msg) {
+    SyntaxError(size_t line, size_t pos, antlr4::Token *token, std::string msg) :
+            line(line), pos(pos), msg(std::move(msg)), hitEOF(token->getType() == antlr4::Token::EOF) {};
+
+    [[nodiscard]] std::string message() const {
         std::stringstream stream;
-        stream << "Syntax error at line " << line << ", pos " << position << ": " << msg;
-        message = stream.str();
-    }
-
-    [[nodiscard]] const char *what() const noexcept override {
-        return message.c_str();
+        stream << "Syntax error at line " << line << " pos " << pos << ": " << msg;
+        return stream.str();
     }
 };
 
@@ -24,6 +24,6 @@ class SyntaxErrorListener : public antlr4::BaseErrorListener {
     void
     syntaxError(antlr4::Recognizer *recognizer, antlr4::Token *offendingSymbol, size_t line, size_t charPositionInLine,
                 const std::string &msg, std::exception_ptr e) override {
-        throw QueryParserSyntaxError(line, charPositionInLine, msg);
+        throw SyntaxError(line, charPositionInLine, offendingSymbol, msg);
     }
 };
