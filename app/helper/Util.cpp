6d509acf1454a2895a1632d06d0ac26d00ab82ce
frothywater <kokoroic@outlook.com>
2021-06-16 17:32:11 +0800

    feat: intersect() overload

--- app/helper/Util.cpp
+++ app/helper/Util.cpp
@@ -4,11 +4,22 @@ void Util::printTable(std::vector<Tuple> tuples, TableInfo table) {
     // TODO: Table formatted output
 }
 
-std::set<int> Util::intersect(const std::set<int> &destination, const std::vector<int> &source) {
+std::set<int> Util::intersect(const std::set<int> &a, const std::vector<int> &b) {
     std::set<int> result;
-    std::vector<int> sortedSource = source;
-    std::sort(sortedSource.begin(), sortedSource.end());
-    std::set_intersection(sortedSource.cbegin(), sortedSource.cend(), destination.cbegin(), destination.cend(),
+    std::vector<int> sortedB = b;
+    std::sort(sortedB.begin(), sortedB.end());
+    std::set_intersection(a.cbegin(), a.cend(), sortedB.cbegin(), sortedB.cend(),
+                          std::inserter(result, result.begin()));
+    return result;
+}
+
+std::set<int> Util::intersect(const std::vector<int> &a, const std::vector<int> &b) {
+    std::set<int> result;
+    std::vector<int> sortedA = a;
+    std::sort(sortedA.begin(), sortedA.end());
+    std::vector<int> sortedB = b;
+    std::sort(sortedB.begin(), sortedB.end());
+    std::set_intersection(sortedA.cbegin(), sortedA.cend(), sortedB.cbegin(), sortedB.cend(),
                           std::inserter(result, result.begin()));
     return result;
 }
--- app/helper/Util.h
+++ app/helper/Util.h
@@ -8,5 +8,7 @@
 namespace Util {
     void printTable(std::vector<Tuple> tuples, TableInfo table);
 
-    static std::set<int> intersect(const std::set<int> &destination, const std::vector<int> &source);
+    static std::set<int> intersect(const std::set<int> &a, const std::vector<int> &b);
+
+    static std::set<int> intersect(const std::vector<int> &a, const std::vector<int> &b);
 }
